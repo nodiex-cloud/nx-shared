71b89dbd408fd1d248c0a9d4a4fb39e0d80ee4ee
Vagif Kuliyev <admin@nodiex.com>
2020-12-03 05:13:54 +0000

    Common deps added.

---
+++ deps/common/create.sh
@@ -0,0 +1,9 @@
+mvn archetype:generate -B \
+   -DarchetypeGroupId=pl.org.miki \
+   -DarchetypeArtifactId=java8-quickstart-archetype \
+   -DarchetypeVersion=1.0.0 \
+   -DgroupId=com.nodiex \
+   -DartifactId=nodiex-deps-common \
+   -Dversion=1.0 \
+   -Dpackage=com.nodiex
+   
